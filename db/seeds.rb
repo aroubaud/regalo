d4229dde64e7e7d1f7b6e186a961dc175cceb0c4
Nancy Ihirwe <kayitarenancy@gmail.com>
2018-11-21 12:14:49 -0500

    created attributes seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+attributes = [
+  {name: 'Sports'}, {name: 'Techie'}, {name: 'Fashionista'}, {name: 'Foodie'},
+  {name: 'Outgoing person'}, {name: 'Male'}, {name: 'Female'}, {name: 'Other'},
+  {name: 'Friend'}, {name: 'Boss'}, {name: 'Colleague'}, {name: 'Family'},
+  {name: 'Kid'}, {name: 'Teen'}, {name: 'Young adult'}, {name: 'Adult'},
+  {name: 'Elderly'}, {name: 'Birthday'}, {name: 'Party'}, {name: 'Wedding'},
+  {name: 'Graduation'}, {name: 'Babyshower'}, {name: 'Christmas'},
+  {name: 'Office party'}, {name: 'Other'}
+]
+
+attributes.each do |attribute|
+  Attribute.create(attribute)
+end
