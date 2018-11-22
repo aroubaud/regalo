98d8f8e67d243ec5b0cf2e458e908ca2813829ad
Alexandre Roubaud <roubaud.alex@gmail.com>
2018-11-22 17:08:28 -0500

    Added question_id column to messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_171252) do
+ActiveRecord::Schema.define(version: 2018_11_22_213725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2018_11_22_171252) do
     t.bigint "facebook_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "question_id"
     t.index ["facebook_user_id"], name: "index_messages_on_facebook_user_id"
   end
 
