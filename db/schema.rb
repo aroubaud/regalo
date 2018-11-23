ec1d3bf77da610aee0105b1e2bcba7dfa9c46f13
luc barika <lucbarika@gmail.com>
2018-11-23 15:50:35 -0500

    fixed bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_164439) do
 ActiveRecord::Schema.define(version: 2018_11_22_220901) do
 
   # These are extensions that must be enabled in order to support this database
