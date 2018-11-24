e6b59e20aebcdcd2590d3dc914cd221d6e90b57b
Alexandre Roubaud <roubaud.alex@gmail.com>
2018-11-24 14:23:50 -0500

    Added skip before action for devise in gifts controller for gifts show page and added bootstrap grid to gifts show page

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,4 +1,6 @@
 class GiftsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def show
     @gift = Gift.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   mount Facebook::Messenger::Server, at: 'bot'
-  resources(:gifts, only:[:show])
+
+  resources :gifts, only:[:show]
 end
