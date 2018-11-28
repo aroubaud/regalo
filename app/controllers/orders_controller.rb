96ff40c10662ca9dfc7ef5b75f117ef752d64c9d
Alexandre Roubaud <roubaud.alex@gmail.com>
2018-11-28 11:19:40 -0500

    Fixed orders show page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,13 +5,14 @@ class OrdersController < ApplicationController
   end
 
   def create
-    gift = Gift.find(params[:gift_id])
-    order = Order.create!(order_params.merge(gift_sku: gift.sku, amount: gift.price, state: 'Pending', user: current_user))
-    redirect_to new_order_payment_path(order)
+    @gift = Gift.find(params[:gift_id])
+    @order = Order.create!(order_params.merge(gift_sku: @gift.sku, amount: @gift.price, state: 'Pending', user: current_user))
+    redirect_to new_order_payment_path(@order)
   end
 
   def show
-    @order = current_user.orders.where(state: 'Paid').find(params[:id])
+    @order = Order.find(params[:id])
+    @gift = Gift.find_by(sku: @order.gift_sku)
   end
 
   private
